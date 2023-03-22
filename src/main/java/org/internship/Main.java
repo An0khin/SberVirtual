22b3368aaa9dfdcdbf6705306bae044e7bb9aabe
An0khin <anohin04@mail.ru>
2023-03-22 19:47:23 +0500

    1 task

---
+++ src/main/java/org/internship/Main.java
@@ -0,0 +1,39 @@
+package org.internship;
+
+import org.internship.model.City;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static final String FILE_PATH = "src/main/resources/Задача ВС Java Сбер.csv";
+
+    public static void main(String[] args) {
+        List<City> cities = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(new File(FILE_PATH))) {
+            while(scanner.hasNextLine()) {
+                String[] fields = scanner.nextLine().split(";");
+
+                String name = fields[1];
+                String region = fields[2];
+                String district = fields[3];
+                int population = Integer.parseInt(fields[4]);
+                String foundation = "";
+
+                if(fields.length > 5) {
+                    foundation = fields[5];
+                }
+
+                City city = new City(name, region, district, population, foundation);
+                cities.add(city);
+            }
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+
+        cities.forEach(System.out::println);
+    }
+}
---
+++ src/main/java/org/internship/model/City.java
@@ -0,0 +1,71 @@
+package org.internship.model;
+
+public class City {
+    private String name;
+    private String region;
+    private String district;
+    private int population;
+    private String foundation;
+
+    public City() {
+    }
+
+    public City(String name, String region, String district, int population, String foundation) {
+        this.name = name;
+        this.region = region;
+        this.district = district;
+        this.population = population;
+        this.foundation = foundation;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", region='" + region + '\'' +
+                ", district='" + district + '\'' +
+                ", population=" + population +
+                ", foundation='" + foundation + '\'' +
+                '}';
+    }
+}
